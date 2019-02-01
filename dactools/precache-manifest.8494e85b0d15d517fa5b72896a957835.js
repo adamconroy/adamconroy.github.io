e9b7d31dac6372b3917ed8954427b66778499a3a
Adam Conroy <adam.conroy@gmail.com>
2019-02-01 03:47:47 -0500

    adding dactools:

---
+++ dactools/precache-manifest.8494e85b0d15d517fa5b72896a957835.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "650719e91b9b0951999a",
+    "url": "/dactools/static/js/runtime~main.650719e9.js"
+  },
+  {
+    "revision": "271c825580384de196f6",
+    "url": "/dactools/static/js/main.271c8255.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/dactools/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "271c825580384de196f6",
+    "url": "/dactools/static/css/main.0764ea60.chunk.css"
+  },
+  {
+    "revision": "58b3144bffc4c5156388fa6bb4aabc05",
+    "url": "/dactools/index.html"
+  }
+];
